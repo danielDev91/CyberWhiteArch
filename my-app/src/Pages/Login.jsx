740b6c1f570ac53f9025bcd2ad9bd14bfb7fd5c0
Daniel <118483762+danielDev91@users.noreply.github.com>
2025-01-23 17:11:06 +0100

    Update Login.jsx
    Adding a random generator numbers

--- my-app/src/Pages/Login.jsx
+++ my-app/src/Pages/Login.jsx
@@ -1,46 +1,13 @@
 import React, { useState } from 'react';
-import logo from '../logo.svg';
-import '../App.css';
 
-const App = () => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-
-    const handleLogin = (e) => {
-        e.preventDefault();
-        // Perform login logic here
-        console.log('Username: ', username);
-        console.log('Password: ', password);
-    };
-    
-    return (
-        <div className='App'>
-            <header className='App-Header'>
-                <img src={logo} className='App-logo' alt="logo" />
-                <p>
-                    Edit <code>src/App.jsx</code> and save to reload.
-                </p>
-                <form onSubmit={handleLogin}>
-                    <div>
-                        <label>Username:</label>
-                        <input 
-                        type="text"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                        />
-                    </div>
-                    <div>
-                        <label>Password:</label>
-                        <input 
-                        type="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        />
-                    </div>
-                </form>
-            </header>
-        </div>
-    );
+const generateRandomNumber = () => {
+    return Math.floor(Math.random() * 10**15).toString().padStart(15, '0');
 };
 
-export default App;
+const Login = () => {
+    const [randomNumber, setRandomNumber] = useState('');
+
+    const handleGenerate = () => {
+        setRandomNumber(generateRandomNumber());
+    };
+};
